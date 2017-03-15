255091bf237a5c66ff06c1a2908409ce011e96eb
Ryan Hoque <ryanhoque2@gmail.com>
2017-03-15 02:05:51 -0700

    completed API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,25 +1,14 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from django.shortcuts import render
+from rest_framework import generics
 from .models import Member
 from .serializers import MemberSerializer
 
-# list members or add members
-class MemberList(APIView):
-	def get(self, request):
-		members = Member.objects.all()
-		serializer = MemberSerializer(members, many=True)
-		return Response(serializer.data)
-	def post(self):
-		pass
+# list members (GET) or add members (POST)
+class MemberList(generics.ListCreateAPIView):
+	queryset = Member.objects.all()
+	serializer_class = MemberSerializer
 
-# def index(request):
-# 	listing = {}
-# 	all_members = Member.objects.all()
-# 	for member in all_members:
-# 		listing[member.id] = str(member)
-# 	return JsonResponse(listing)
-
-# def delete(request, api_id):
-# 	return HttpResponse("")
+# update members (PATCH) or delete members (DELETE)
+class MemberDetail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Member.objects.all()
+	serializer_class = MemberSerializer
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,8 +5,10 @@ from api import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', views.MemberList.as_view()),
-    url(r'^api/(?P<pk>[0-9]+)$', views.MemberList.as_view()),
+    # Endpoint to list or create members is /api/
+    url(r'^api/?$', views.MemberList.as_view()),
+    # Endpoint to update or delete members is /api/[ID]/
+    url(r'^api/(?P<pk>[0-9]+)/?$', views.MemberDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
