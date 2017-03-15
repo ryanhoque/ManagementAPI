f95bce61de869818169bf3aebe35790a75f71bd9
Ryan Hoque <ryanhoque2@gmail.com>
2017-03-15 01:42:52 -0700

    added RESTful architecture

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,25 @@
-from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Member
-import json
+from .serializers import MemberSerializer
 
-def index(request):
-	listing = {}
-	all_members = Member.objects.all()
-	for member in all_members:
-		listing[member.id] = str(member)
-	return JsonResponse(listing)
+# list members or add members
+class MemberList(APIView):
+	def get(self, request):
+		members = Member.objects.all()
+		serializer = MemberSerializer(members, many=True)
+		return Response(serializer.data)
+	def post(self):
+		pass
 
-def delete(request, api_id):
-	return HttpResponse("")
+# def index(request):
+# 	listing = {}
+# 	all_members = Member.objects.all()
+# 	for member in all_members:
+# 		listing[member.id] = str(member)
+# 	return JsonResponse(listing)
+
+# def delete(request, api_id):
+# 	return HttpResponse("")
